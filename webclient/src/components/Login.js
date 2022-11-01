e30f561c5eb91412a17a608f22a29d0d03745d78
sandeep <ndys699@gmail.com>
2022-11-01 14:10:42 +0530

    added login

--- webclient/src/components/Login.js
+++ webclient/src/components/Login.js
@@ -2,21 +2,25 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>
-        <form>
-            <div>
-           <label></label>
-           <input placeholder="Username"></input>
-           </div>
-           <div>
-            <label></label>
-            <input placeholder="Password"></input>
-            </div>
-            <div>
-            <button>Login</button> 
-           </div>
-        </form>
-    </div>
+    
+        <div className='bodyLayout'>
+      <img className='logo' src="./logoBRN.png"></img>
+      <div className='signInForm alignCenter'>
+          <h1 className='text-SignIn'>Sign In</h1>
+          <div>
+              <input type='email' className='input' placeholder='Enter your Email'></input>
+              <input type='password' className='input'placeholder='Password'></input>
+          </div>
+          <div className='flex-Row alignCenter'>
+              <button className='button-SignIn'>LOGIN</button>
+              <div className='label-CheckBox'>
+                  <input type="checkbox" htmlFor='rem'className='input-CheckBox'/>
+                  <label id='rem' className='signInText'>Remember</label>
+              </div>
+              </div>
+              </div>
+              </div>
+   
   )
 }
 
