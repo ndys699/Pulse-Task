4fca9596d1f8e4d41ac7f84863943584b94d3b1c
sandeep <ndys699@gmail.com>
2022-11-01 14:02:20 +0530

    added components

---
+++ webclient/src/components/Login.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+        <form>
+            <div>
+           <label></label>
+           <input placeholder="Username"></input>
+           </div>
+           <div>
+            <label></label>
+            <input placeholder="Password"></input>
+            </div>
+            <div>
+            <button>Login</button> 
+           </div>
+        </form>
+    </div>
+  )
+}
+
+export default Login
