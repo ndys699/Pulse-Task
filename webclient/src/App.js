de71816e181525e8dff190405201f7c4de34749b
sandeep <ndys699@gmail.com>
2022-11-01 16:16:05 +0530

    linked attendance

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
-
+import Attendance from './components/Attendance'
 
 import {BrowserRouter,Routes,Route,Link} from "react-router-dom"
 
@@ -14,10 +14,12 @@ function App() {
  <nav>
   <Link to="/">Login</Link>
   <Link to="/SignUp">SignUp</Link>
+  <Link to="/Attendance">Attendance</Link>
  </nav>
  <Routes>
   <Route path="/"element={<Login></Login>}></Route>
   <Route path="/SignUp" element={<SignUp></SignUp>}></Route>
+  <Route path="/Attendance" element={<Attendance></Attendance>}></Route>
  </Routes>
  </BrowserRouter>
 
