813a48503b929093840df9ee28cccfe2f97fd4d6
sandeep <ndys699@gmail.com>
2022-11-01 15:57:46 +0530

    added signup

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import SignUp from './components/SignUp';
 import {BrowserRouter,Routes,Route,Link} from "react-router-dom"
 
 function App() {
