dc5c253a624645c26ffd0dd47350580fb6fa7622
sandeep <ndys699@gmail.com>
2022-11-01 15:54:10 +0530

    added react-router-dom

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -1,16 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import {BrowserRouter,Routes,Route,Link} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
- 
+ <BrowserRouter>
+ <nav>
+  <Link to="/">Login</Link>
+  <Link to="/SignUp">SignUp</Link>
+ </nav>
+ <Routes>
+  <Route path="/"element={<Login></Login>}></Route>
+  <Route path="/SignUp" element={<SignUp></SignUp>}></Route>
+ </Routes>
+ </BrowserRouter>
 
 
 
 
-      <Login></Login>
+      
       
       
 
