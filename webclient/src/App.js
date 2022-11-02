0ce1bc84d9ac521ad66ba4a7d1e2a2448de846c6
sandeep <ndys699@gmail.com>
2022-11-02 12:14:27 +0530

    renamed login

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
-import Attendance from './components/Attendance'
-
-import {BrowserRouter,Routes,Route,Link} from "react-router-dom"
+import Attendance from './components/Attendance';
+import CreateRequest from './components/CreateRequest';
+import LeaveRequest from './components/LeaveRequest';
+import DailyStatusUpdate from './components/DailyStatusUpdate';
+import {BrowserRouter,Routes,Route,Link} from "react-router-dom";
 
 
 function App() {
@@ -12,14 +14,20 @@ function App() {
     <div className="App">
  <BrowserRouter>
  <nav>
-  <Link to="/">Login</Link>
+  <Link to="/">Loginn</Link>
   <Link to="/SignUp">SignUp</Link>
   <Link to="/Attendance">Attendance</Link>
+  <Link to="/Create Request">Create a Request</Link>
+  <Link to="/Leave Request">Leave Request</Link>
+  <Link to="/Daily Status Update">Daily Status Update</Link>
  </nav>
  <Routes>
   <Route path="/"element={<Login></Login>}></Route>
   <Route path="/SignUp" element={<SignUp></SignUp>}></Route>
   <Route path="/Attendance" element={<Attendance></Attendance>}></Route>
+  <Route path="/Create Request" element={<CreateRequest></CreateRequest>}></Route>
+  <Route path="/Leave Request" element={<LeaveRequest></LeaveRequest>}></Route>
+  <Route path="/Daily Status Update" element={<DailyStatusUpdate></DailyStatusUpdate>}></Route>
  </Routes>
  </BrowserRouter>
 
